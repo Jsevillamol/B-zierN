ed34a436d382ab3c7f74b284502fc1a0b8ff46a6
Jsevillamol <jsevillamol@outlook.com>
2013-12-15 19:26:16 +0100

    Degree elevation debugged

--- degree elevation.py
+++ degree elevation.py
@@ -1,7 +1,8 @@
 def degree_elevation(control_points):
         n = len(control_points)
         #El primer y último punto de control coinciden con el primer y ultimo punto de control de la nueva curva
-        control_points.append(control_points[-1])
+        last_point = control_points[-1][:]
+        control_points.append(last_point)
         print control_points
         #El resto de puntos son calculados
         for i in xrange(1, n):
@@ -9,5 +10,3 @@ def degree_elevation(control_points):
                 control_points[i][1]=(float(i)/float(n+1))*control_points[i-1][1]+(1-(float(i)/float(n+1)))*control_points[i][1]
                 print control_points
         return control_points
-
-test = [[0,0],[10,10],[24,69],[74,0],[58,58],[0,100]]
