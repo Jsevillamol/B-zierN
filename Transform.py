5bc57973eb9c7502124b0536187df6644b933f30
Jsevillamol <jsevillamol@outlook.com>
2013-12-24 15:27:14 +0100

    Added rotation

--- Transform.py
+++ Transform.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import math
+
+from auxiliary_functions import *
+
 # Applies affine transformations
-def transform(control_points, scalar = 1, vector = [0,0] ):
+def transform(control_points, scalar=1, vector=[0,0]):
     for i in range(len(control_points)):
         # Scalates
         control_points[i][0] *= scalar
@@ -10,4 +14,14 @@ def transform(control_points, scalar = 1, vector = [0,0] ):
         # Translates
         control_points[i][0] += vector[0]
         control_points[i][1] += vector[1]
-	return control_points
+    return control_points
+
+# Rotates with center in [0,0]
+def rotate (control_points, angle=0):
+    new_control_points = [[0,0]]*len(control_points)
+    for i in range(len(control_points)):
+        new_control_points[i][0] = (control_points[i][0]*math.cos(angle) +
+                                    control_points[i][1]*math.sin(angle))
+        new_control_points[i][1] = (control_points[i][1]*math.cos(angle) -
+                                    control_points[i][0]*math.sin(angle))
+    return new_control_points
